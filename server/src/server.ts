e64b9da7a30817d9ccfb19d6c36c76da528a6c62
CocoCondo <leonardo.conde@correo.ucu.edu.uy>
2023-10-30 20:16:41 -0300

    feat/mongoose

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,21 @@
 import userApi from "./user-api.ts";
+import adminApi from "./admin-api.ts"
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from "body-parser";
 
 var app = express();
+const portDB = 3000;
 
-app.use("/rooms", userApi),
+app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-    res.send('Hello World');
-})
+//Conectar a MongoDB
+mongoose.connect('mongodb://localhost:27017/base_asadility');
+
+//Rutas de la API
+//console.log(adminApi);
+app.use("/api", userApi);
+app.use("/api", adminApi);
 
 var server = app.listen(8080, function() {
     console.log("Backend Application listening at http://localhost:8080")
