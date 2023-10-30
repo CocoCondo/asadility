9e145f29ce210429e558a22e049be6c85ce323a0
CocoCondo <leonardo.conde@correo.ucu.edu.uy>
2023-10-30 20:16:41 -0300

    feat/mongoose

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,29 @@
 import express, { Request, Response } from 'express';
 import userApi from "./user-api.ts";
 import authentication from "./authentication.ts";
+import adminApi from "./admin-api.ts"
+import mongoose from 'mongoose';
+import bodyParser from "body-parser";
 
 var app = express();
 app.use(express.json());
+app.use(bodyParser.json());
 
 const cors = require('cors');
 app.use(cors({
     origin: ['http://localhost:4200', 'http://127.0.0.1:4200']
 }));
 
-app.use("/", userApi);
 app.use(authentication);
+
+const portDB = 3000;
+//Conectar a MongoDB
+mongoose.connect('mongodb://localhost:27017/base_asadility');
+
+//Rutas de la API
+//console.log(adminApi);
+app.use("/api", userApi);
+app.use("/api", adminApi);
 
 // app.get('/', function(req: Request, res: Response) {
 //     res.send('Hello World')
--- server/src/user-api.ts
+++ server/src/user-api.ts
@@ -9,18 +9,18 @@ let rooms = [];
 router.post('/rooms', (req, res) => {
     const room = req.body;
     rooms.push(room);
-    console.log(room);
+    //console.log(room);
     res.send('Room was added to the database');
 });
 
 //Agregar users a un room
 router.post('/room', (req,res) => {
     const user = req.body;
-    console.log(user);
+    //console.log(user);
 });
 
 router.get('/', (req,res) => {
-    console.log("Hace get",req);
+    //console.log("Hace get",req);
     res.send('Funciona el get');
 });
 
