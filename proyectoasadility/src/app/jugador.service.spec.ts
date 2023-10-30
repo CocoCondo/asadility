7f4d15735c755cb216729802ac03176e68b77977
Javier Odera <javier.odera@correo.ucu.edu.uy>
2023-10-30 20:04:09 -0300

    Lobby

---
+++ proyectoasadility/src/app/jugador.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { JugadorService } from './jugador.service';
+
+describe('JugadorService', () => {
+  let service: JugadorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(JugadorService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
