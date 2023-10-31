9f27d3a521d74055178e8225b783e74789e4b03b
Cotidib <constanza.dibueno@gmail.com>
2023-10-31 17:38:46 -0300

    autenticacion

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,8 +14,6 @@ app.use(cors({
     origin: ['http://localhost:4200', 'http://127.0.0.1:4200']
 }));
 
-app.use(authentication);
-
 const portDB = 3000;
 //Conectar a MongoDB
 mongoose.connect('mongodb://localhost:27017/base_asadility');
@@ -24,6 +22,7 @@ mongoose.connect('mongodb://localhost:27017/base_asadility');
 //console.log(adminApi);
 app.use("/api", userApi);
 app.use("/api", adminApi);
+app.use(authentication);
 
 // app.get('/', function(req: Request, res: Response) {
 //     res.send('Hello World')
