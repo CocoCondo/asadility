341e43641e64c1ecf98b6a86ea3d0099ebb7db2c
Cotidib <constanza.dibueno@gmail.com>
2023-10-31 19:32:40 -0300

    auth interceptor

--- proyectoasadility/src/app/app.module.ts
+++ proyectoasadility/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -9,6 +8,9 @@ import { FormsModule } from '@angular/forms';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FooterComponent } from './footer/footer.component';
 import { LobbyComponent } from './lobby/lobby.component';
+
+// Interceptors
+import { AuthinterceptorService } from './authinterceptor.service';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,14 @@ import { LobbyComponent } from './lobby/lobby.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    // comentado para que no se active la intercepcion de requests con jwt
+    // {
+    //   provide: HTTP_INTERCEPTORS,
+    //   useClass: AuthinterceptorService,
+    //   multi: true
+    // }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
