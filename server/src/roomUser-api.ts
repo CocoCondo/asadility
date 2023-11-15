c9779ddfc3ddf6561fc6029b0d00a1a4788b1963
Cotidib <constanza.dibueno@gmail.com>
2023-11-15 20:21:55 -0300

    crear room con codigo

--- server/src/roomUser-api.ts
+++ server/src/roomUser-api.ts
@@ -57,11 +57,6 @@ router.patch('/rooms/:code/removePlayers', async (req, res) => {
     }
   });
 
-router.get('/', (req, res) => {
-    //console.log("Hace get",req);
-    res.send('Funciona el get');
-});
-
 router.get('/rooms/:code/users', async function(req, res) {
     const roomCode = req.params.code;
   try{
