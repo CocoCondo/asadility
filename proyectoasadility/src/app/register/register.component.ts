8d1051f314f7d48594d97911c669dc48f53a68af
JavierOdera <javier.odera@correo.ucu.edu.uy>
2023-11-26 17:35:20 -0300

    Mensajes de exito

--- proyectoasadility/src/app/register/register.component.ts
+++ proyectoasadility/src/app/register/register.component.ts
@@ -4,29 +4,37 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
   username: string = '';
   password: string = '';
+  mostrarMensaje: boolean = false;
+  mensaje: string =
+    'Ta has registrado! Vuelve para atras y loggearte con tu nueva cuenta';
 
-  constructor(private authService: AuthService){};
+  constructor(private authService: AuthService) {}
 
   crearNuevoUsuario() {
     const datosUsuario = {
       username: this.username,
-      password:this.password
+      password: this.password,
     };
 
     this.authService.crearUsuario(datosUsuario).subscribe(
       (respuesta) => {
         console.log('Usuario creado con éxito', respuesta);
+        if (datosUsuario.username != '' && datosUsuario.password != '') {
+          this.mostrarMensaje = true;
+          // Oculta el mensaje después de 3 segundos (3000 milisegundos)
+          setTimeout(() => {
+            this.mostrarMensaje = false;
+          }, 3000);
+        }
       },
       (error) => {
         console.error('Error al crear usuario', error);
       }
     );
   }
-  
-
 }
